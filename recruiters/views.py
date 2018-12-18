6f8bca494efbd3d6431b6fd1a15a01ef0cfb651c
saikarthik1211 <saikarthik1211@gmail.com>
2018-12-18 18:56:14 +0530

    backend complete

--- recruiters/views.py
+++ recruiters/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
-
+from .models import WhyUs,VisitedCompanies,Policy,ProcedureStep,PolicyFileUpload,PlacementFileUpload,ChartData,StatsTable
 # Create your views here.
 def pastplacements(request):
-    return render(request, 'pastplacements.html')
+    files = PlacementFileUpload.objects.all()
+    chartdata=ChartData.objects.all()
+    statsdata=StatsTable.objects.all()
+    count=files.count()
+    return render(request, 'pastplacements.html',locals())
 
 def pastrecruiters(request):
-    return render(request, 'pastrecruiters.html')
+    comp=VisitedCompanies.objects.all().order_by('name')
+    return render(request, 'pastrecruiters.html',locals())
 
 def whyus(request):
-    return render(request, 'whyus.html')
+    whyus=WhyUs.objects.all()
+    whyus=whyus[0]
+    return render(request, 'whyus.html',locals())
 
 def pandp(request):
-    return render(request, 'pandp.html')
+    policy=Policy.objects.all()
+    policy=policy[0]
+    proceduresteps=ProcedureStep.objects.all()
+    files=PolicyFileUpload.objects.all()
+    return render(request, 'pandp.html',locals())
